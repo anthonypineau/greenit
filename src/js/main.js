5d872fb1c685687c86d8280e32c633ff7fb8b244
Anthony Pineau <anthonypineau85@gmail.com>
2021-10-17 20:43:20 +0200

    commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+console.log("test");
+const selectTransportation=document.querySelector('#transportation');
+selectTransportation.addEventListener("change", () => {
+    //document.querySelector("#transportationQuestion").textContent="Nombre de kilomètres parcourus pour le travail (par an)";
+    console.log("test change");
+});
