779fb5ae7a70024841634c190e8691bb9b5052a5
Anthony Pineau <anthonypineau85@gmail.com>
2021-10-18 23:02:00 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,37 @@
-console.log("test");
+//const dataFile = require("../../data/data.json");
+//const data= JSON.parse(dataFile);
+//console.log(data);
+
 const selectsTransportation=document.querySelectorAll('.transportation');
+
+fetch("../../data/data.json")
+.then(response => {
+   return response.json();
+})
+.then(data => {
+    selectsTransportation.forEach(e => {
+        data.forEach(d => {
+            const option = document.createElement("option");
+            option.textContent=d.description;
+            option.value=d.value;
+            e.appendChild(option);
+        });
+        e.addEventListener("change", () => {
+            switch(e.id){
+                case "transportation1":
+                    calculateTranport(transportation1, transport1, transport_hours1);
+                    break;
+                case "transportation2":
+                    calculateTranport(transportation2, transport2, transport_hours2);
+                    break;
+                case "transportation3":
+                    calculateTranport(transportation3, transport3, transport_hours3);
+                    break;
+            }
+        });
+    });
+});
+
 const transport_hours=document.querySelectorAll('.transport_hours');
 const transport1=document.querySelector('#transport1');
 const transport2=document.querySelector('#transport2');
@@ -21,87 +53,24 @@ inputsComputer.forEach((e) => {
     });
 });
 
-
-selectsTransportation.forEach((e) => {
-    e.addEventListener("change", () => {
-        switch(e.value){
-            case "car":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre de kilomètres parcourus pour le travail (par an)";
-                break;
-            case "bike":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre de kilomètres parcourus pour le travail (par an)";
-                break;
-            case "plane":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre d'heures d'utilisation pour le travail (par an)";
-                break;
-            case "bus":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre d'heures d'utilisation pour le travail (par semaine)";
-                break;
-            case "train":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre de kilomètres parcourus pour le travail (par an)";
-                break;
-            case "subway":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre d'heures d'utilisation pour le travail (par semaine)";
-                break;
-            case "bicycle":
-                e.parentElement.parentElement.parentElement.children[1].children[0].textContent="Nombre de kilomètres parcourus pour le travail (par an)";
-                break;
-        }
-        switch(e.id){
-            case "transportation1":
-                calculateTranport(transportation1, transport1, transport_hours1.value);
-                break;
-            case "transportation2":
-                calculateTranport(transportation2, transport2, transport_hours2.value);
-                break;
-            case "transportation3":
-                calculateTranport(transportation3, transport3, transport_hours3.value);
-                break;
-        }
-    });
-});
-
 transport_hours.forEach((e) => {
     e.addEventListener("change", () => {
         switch(e.id){
             case "transport_hours1":
-                calculateTranport(transportation1, transport1, e.value);
+                calculateTranport(transportation1, transport1, transport_hours1);
                 break;
             case "transport_hours2":
-                calculateTranport(transportation2, transport2, e.value);
+                calculateTranport(transportation2, transport2, transport_hours2);
                 break;
             case "transport_hours3":
-                calculateTranport(transportation3, transport3, e.value);
+                calculateTranport(transportation3, transport3, transport_hours3);
                 break;
         }
     });
 });
 
 function calculateTranport(selectTransportation, display, value){
-    let result=0;
-    switch(selectTransportation.value){
-        case "car":
-            result=(280+value*0.2)/1.2;
-            break;
-        case "bike":
-            result=0.06*value;
-            break;
-        case "plane":
-            result=(561.3*value)*0.19;
-            break;
-        case "bus":
-            result=(1.99*value)*52;
-            break;
-        case "train":
-            result=0.01*value;
-            break;
-        case "subway":
-            result=(0.17*value)*52;
-            break;
-        case "bicycle":
-            result=0;
-            break;
-    }
+    let result=selectTransportation.value*value.value;
     result=result.toFixed(2);
     display.textContent=result;
     transport_total.textContent=(Number.parseFloat(transport1.textContent)+Number.parseFloat(transport2.textContent)+Number.parseFloat(transport3.textContent)).toFixed(2);
